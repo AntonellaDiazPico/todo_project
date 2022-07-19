8df53362dcd57670f6791958d5f85524f646bb10
AntonellaDiazPico <antonelladiazpico@gmail.com>
2022-07-20 09:16:45 +1000

    Rustic front end code added

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "todoproject": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
+import {API} from 'aws-amplify';
+import * as queries from './graphql/queries';
+import * as mutations from './graphql/mutations';
 
 function App() {
+
+  async function fetchTodos() {
+
+    let response = await API.graphql({
+      query: queries.listTodos
+    })
+    console.log(response);
+  }
+  
+  async function createTodo() {
+      let myTodo = {
+        name: "Todo 4",
+        description: "some description"
+      };
+      let response = await API.graphql({
+        query: mutations.createTodo,
+        variables: {
+          input: myTodo
+        }
+      });
+
+      console.log(response);
+  }
+
   return (
     <div>
       <h1>
         Hello world
       </h1>
+      <button onClick={fetchTodos}>List todos</button>
+      <button onClick={createTodo}>Create todos</button>
     </div>
   );
 }
